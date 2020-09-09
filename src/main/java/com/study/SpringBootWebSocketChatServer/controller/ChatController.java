d2ac6fad2fb6dcce04ebba3371e5d65163bbc060
Alex Won <dnjscksdn98@gmail.com>
2020-09-09 18:34:21 +0900

    feat: create publisher

--- src/main/java/com/study/SpringBootWebSocketChatServer/controller/ChatController.java
+++ src/main/java/com/study/SpringBootWebSocketChatServer/controller/ChatController.java
@@ -1,26 +1,28 @@
 package com.study.SpringBootWebSocketChatServer.controller;
 
-import com.study.SpringBootWebSocketChatServer.model.ChatRoom;
-import com.study.SpringBootWebSocketChatServer.service.ChatService;
+import com.study.SpringBootWebSocketChatServer.model.ChatMessage;
+import com.study.SpringBootWebSocketChatServer.model.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/chat")
 public class ChatController {
 
     @Autowired
-    private ChatService chatService;
+    private SimpMessageSendingOperations messagingTemplate;
 
-    @PostMapping
-    public ChatRoom createChatRoom(@RequestParam String name) {
-        return chatService.createChatRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> getAllChatRoom() {
-        return chatService.findAllChatRoom();
+    /**
+     *  Description:
+     *      - @MessageMapping 을 통해 WebSocket 으로 들어오는 메시지 발행을 처리합니다.
+     *
+     */
+    @MessageMapping(value = "/chat/message")
+    public void send(ChatMessage message) {
+        if (MessageType.ENTER.equals(message.getMessageType())) {
+            message.setMessage(message.getSender() + " 님이 입장하셨습니다.");
+        }
+        messagingTemplate.convertAndSend("/sub/chat/room/" + message.getChatRoomId(), message);
     }
 }
