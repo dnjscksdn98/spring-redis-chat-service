1f8774ffb9d8acb16d915ad0d3845f5be4e0ae3b
Alex Won <dnjscksdn98@gmail.com>
2020-09-09 18:11:38 +0900

    chore: delete session in chat room

--- src/main/java/com/study/SpringBootWebSocketChatServer/model/ChatRoom.java
+++ src/main/java/com/study/SpringBootWebSocketChatServer/model/ChatRoom.java
@@ -1,16 +1,9 @@
 package com.study.SpringBootWebSocketChatServer.model;
 
-import com.study.SpringBootWebSocketChatServer.service.ChatService;
-import org.springframework.web.socket.WebSocketSession;
-
-import java.util.HashSet;
-import java.util.Set;
-
 public class ChatRoom {
 
     private String id;
     private String name;
-    private Set<WebSocketSession> sessions = new HashSet<>();
 
     protected ChatRoom() {
 
@@ -23,20 +16,6 @@ public class ChatRoom {
 
     public static ChatRoom of(String id, String name) {
         return new ChatRoom(id, name);
-    }
-
-    public void handleAction(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
-        if (chatMessage.getMessageType().equals(MessageType.ENTER)) {
-            sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + " 님이 입장했습니다.");
-        }
-        sendMessage(chatMessage, chatService);
-    }
-
-    private void sendMessage(ChatMessage chatMessage, ChatService chatService) {
-        sessions
-                .parallelStream()
-                .forEach(session -> chatService.sendMessage(session, chatMessage));
     }
 
     public String getId() {
@@ -53,13 +32,5 @@ public class ChatRoom {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Set<WebSocketSession> getSessions() {
-        return sessions;
-    }
-
-    public void setSessions(Set<WebSocketSession> sessions) {
-        this.sessions = sessions;
     }
 }
