23a7297fc231f9743a96fbfa0ce36dbfed6dcde7
Alex Won <dnjscksdn98@gmail.com>
2020-09-09 21:43:42 +0900

    feat: use redis pub/sub

---
+++ src/main/java/com/study/SpringBootWebSocketChatServer/config/RedisConfig.java
@@ -0,0 +1,39 @@
+package com.study.SpringBootWebSocketChatServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    /**
+     *  Description:
+     *      - Redis Pub/Sub 메시지를 처리하는 Listener 설정
+     *
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
+    /**
+     *  Description:
+     *      - 애플리케이션에서 사용할 RedisTemplate 설정
+     *
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+        return redisTemplate;
+    }
+}
--- src/main/java/com/study/SpringBootWebSocketChatServer/repository/ChatRoomRepository.java
+++ src/main/java/com/study/SpringBootWebSocketChatServer/repository/ChatRoomRepository.java
@@ -1,7 +1,12 @@
 package com.study.SpringBootWebSocketChatServer.repository;
 
 import com.study.SpringBootWebSocketChatServer.model.ChatRoom;
+import com.study.SpringBootWebSocketChatServer.redis.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -9,26 +14,60 @@ import java.util.*;
 @Repository
 public class ChatRoomRepository {
 
-    private Map<String, ChatRoom> chatRooms;
+    private RedisSubscriber redisSubscriber;  // 구독 처리 서비스
+    private RedisMessageListenerContainer redisMessageListener;  // 채팅방(Topic)에 발행되는 메시지를 처리할 Listener
+
+    private static final String CHAT_ROOMS = "CHAT_ROOM";
+    private RedisTemplate<String, Object> redisTemplate;
+    private HashOperations<String, String, ChatRoom> hashChatRoom;
+    private Map<String, ChannelTopic> topics;
 
     @Autowired
-    public ChatRoomRepository() {
-        this.chatRooms = new LinkedHashMap<>();
+    public ChatRoomRepository(
+            RedisSubscriber redisSubscriber,
+            RedisMessageListenerContainer redisMessageListener,
+            RedisTemplate<String, Object> redisTemplate
+    ) {
+        this.hashChatRoom = redisTemplate.opsForHash();
+        this.topics = new HashMap<>();
+        this.redisSubscriber = redisSubscriber;
+        this.redisMessageListener = redisMessageListener;
+        this.redisTemplate = redisTemplate;
     }
 
     public List<ChatRoom> findAll() {
-        List<ChatRoom> findAll = new ArrayList<>(chatRooms.values());
-        Collections.reverse(findAll);
-        return findAll;
+        return hashChatRoom.values(CHAT_ROOMS);
     }
 
     public ChatRoom findById(String id) {
-        return chatRooms.get(id);
+        return hashChatRoom.get(CHAT_ROOMS, id);
     }
 
+    /**
+     *  Description:
+     *      - 채팅방 생성: 서버간 채팅방 공유를 위해 Redis Hash에 저장합니다.
+     *
+     */
     public ChatRoom save(String name) {
         ChatRoom chatRoom = ChatRoom.of(UUID.randomUUID().toString(), name);
-        chatRooms.put(chatRoom.getId(), chatRoom);
+        hashChatRoom.put(CHAT_ROOMS, chatRoom.getId(), chatRoom);
         return chatRoom;
+    }
+
+    /**
+     *  Description:
+     *      - Redis에 Topic을 만들고 pub/sub 통신을 하기 위해 Listener를 설정합니다.
+     */
+    public void enterChatRoom(String id) {
+        ChannelTopic topic = topics.get(id);
+        if (topic == null) {
+            topic = new ChannelTopic(id);
+            redisMessageListener.addMessageListener(redisSubscriber, topic);
+            topics.put(id, topic);
+        }
+    }
+
+    public ChannelTopic getTopic(String id) {
+        return topics.get(id);
     }
 }
