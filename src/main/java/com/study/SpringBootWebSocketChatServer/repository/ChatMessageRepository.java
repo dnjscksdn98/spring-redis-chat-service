726cb0820373f8be98f7e8feb8de36580f70cafd
Alex Won <dnjscksdn98@gmail.com>
2020-09-12 22:21:41 +0900

    chore: switch to jpa repo

--- src/main/java/com/study/SpringBootWebSocketChatServer/repository/ChatMessageRepository.java
+++ src/main/java/com/study/SpringBootWebSocketChatServer/repository/ChatMessageRepository.java
@@ -1,33 +1,11 @@
 package com.study.SpringBootWebSocketChatServer.repository;
 
 import com.study.SpringBootWebSocketChatServer.model.ChatMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
-@Repository
-public class ChatMessageRepository {
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
-    private Map<String, ChatMessage> chatMessages;
-
-    @Autowired
-    public ChatMessageRepository() {
-        this.chatMessages = new LinkedHashMap<>();
-    }
-
-    public List<ChatMessage> findAll(String chatRoomId) {
-        return chatMessages.values()
-                .stream()
-                .filter(msg -> msg.getChatRoomId().contentEquals(chatRoomId))
-                .collect(Collectors.toList());
-    }
-
-    public ChatMessage save(ChatMessage message) {
-        chatMessages.put(message.getChatRoomId(), message);
-        return message;
-    }
+    List<ChatMessage> findAllByChatRoomId(Long id);
 }
