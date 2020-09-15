16db8f2db47511907147174f900df036aa47febe
Alex Won <dnjscksdn98@gmail.com>
2020-09-15 14:48:42 +0900

    chore: add chat room payload object

---
+++ src/main/java/com/study/SpringBootWebSocketChatServer/model/ChatRoomPayload.java
@@ -0,0 +1,28 @@
+package com.study.SpringBootWebSocketChatServer.model;
+
+public class ChatRoomPayload {
+
+    private Long id;
+    private String name;
+
+    public ChatRoomPayload(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
