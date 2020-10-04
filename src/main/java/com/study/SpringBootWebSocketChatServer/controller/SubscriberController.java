02347ce01b62c6b08758b991989de3918d37d1b2
Alex Won <dnjscksdn98@gmail.com>
2020-10-05 00:22:20 +0900

    feat: add message listener

--- src/main/java/com/study/SpringBootWebSocketChatServer/controller/SubscriberController.java
+++ src/main/java/com/study/SpringBootWebSocketChatServer/controller/SubscriberController.java
@@ -1,10 +1,13 @@
 package com.study.SpringBootWebSocketChatServer.controller;
 
 import com.study.SpringBootWebSocketChatServer.domain.model.ChatRoom;
+import com.study.SpringBootWebSocketChatServer.redis.RedisSubscriber;
 import com.study.SpringBootWebSocketChatServer.repository.ChatMessageRepository;
 import com.study.SpringBootWebSocketChatServer.repository.ChatRoomRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +30,12 @@ public class SubscriberController {
     @Autowired
     private ChatMessageRepository chatMessageRepository;
 
+    @Autowired
+    private RedisSubscriber redisSubscriber;
+
+    @Autowired
+    private RedisMessageListenerContainer redisMessageListener;  // 채팅방(Topic)에 발행되는 메시지를 처리할 Listener(Subscriber)
+
     /**
      *  Description:
      *      - 채팅방 이름을 요청 파라미터로 전달해서 채팅방 개설
@@ -37,6 +46,8 @@ public class SubscriberController {
         ChatRoom newChatRoom = ChatRoom.getBuilder()
                 .withName(name)
                 .build();
+        ChannelTopic topic = ChannelTopic.of(newChatRoom.getId().toString());
+        redisMessageListener.addMessageListener(redisSubscriber, topic);
         return chatRoomRepository.save(newChatRoom);
     }
 
